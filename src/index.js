5028dae5abea40021a50dbb11c5b54a9e761b365
stephenunder <41934385+stephenunder@users.noreply.github.com>
2019-02-18 20:53:18 -0600

    install redux and move searchField state to it

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import "tachyons";
 
 import * as serviceWorker from './serviceWorker';
 
+import { applyMiddleware, createStore } from "redux";
+
 import App from "./containers/App";
+import { Provider } from "react-redux";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createLogger } from "redux-logger";
+import { searchRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
